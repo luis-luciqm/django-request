7a0d85d5682e83fe930c3f7c4665a34bbed99009
luis-luciqm <redbullfc18@gmail.com>
2023-10-18 11:09:45 -0300

    organizando o codigo

--- analytics/api/v1/urls.py
+++ analytics/api/v1/urls.py
@@ -4,5 +4,6 @@ from .viewsets import *
 
 urlpatterns = [
     path('register_analytics/', RequestPechinchouSaveViewSet.as_view(), name='register_analytics'),
+    path('view_access_lastminute/', ViewAndAgroupRoutesByDetailOfProductMostAccessedInAPPLastMinute.as_view(), name = 'view_access_lastminute'),
 
 ]
