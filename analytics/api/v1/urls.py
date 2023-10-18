85be106fa83bc6caba7cf86453dd4174b39cf707
luis-luciqm <redbullfc18@gmail.com>
2023-10-18 11:06:49 -0300

    organizando o codigo

--- analytics/api/v1/urls.py
+++ analytics/api/v1/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .viewsets import *
 
 urlpatterns = [
-    # Product
-    path('register_analytics/', RequestPechinchouSaveViewSet.as_view(), name='cadastrar_produto'),
+    path('register_analytics/', RequestPechinchouSaveViewSet.as_view(), name='register_analytics'),
 
 ]
--- analytics/api/v1/viewsets.py
+++ analytics/api/v1/viewsets.py
@@ -1,6 +1,9 @@
 from rest_framework.views import APIView, Response
 
 from request.models import Request
+from django.utils import timezone
+from datetime import timedelta
+from django.db.models import Count
 
 class RequestPechinchouSaveViewSet(APIView):
 
@@ -13,3 +16,20 @@ class RequestPechinchouSaveViewSet(APIView):
             return Response({'message': 'Salvo com sucesso!'})
         except Exception as e:
             return Response({'error': f'Ocorreu um erro ao salvar: {e}'})
+        
+class ViewAndAgroupRoutesByDetailOfProductMostAccessedInAPPLastMinute(APIView):
+
+    def get(self, request, *args, **kwargs):
+
+        # TransactionAmazon.objects.filter(data__date = endDate).values('nome').annotate(data = Max('data'), categoria = Max('categoria'), dispositivo = Max('dispositivo'), price = Max('price'), qtd_pedidos = Max('qtd_pedidos'), quantity = Count('nome')).order_by('-quantity')
+
+        # preciso pegar todas as requests do ultimo minuto
+        requests = Request.objects \
+            .filter(path__icontains = '/api/v3/app/produto/list-detail-of-product/'
+            , time__gte = (timezone.now() - timedelta(minutes = 1))) \
+            .annotate(users = Count('path')) \
+            .order_by('-quantity')
+        
+        print(requests)
+
+        return Response('OK')
